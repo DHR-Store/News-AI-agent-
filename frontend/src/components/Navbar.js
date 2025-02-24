280a3ba690fa113af01d5a50df94a4bc6d84a5db
DHR-Store <kalipusur1234@gmail.com>
2025-02-24 11:27:24 +0530

    Create Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Navbar = () => {
+  return (
+    <motion.nav
+      initial={{ y: -50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className="navbar"
+    >
+      <h1>News Aggregator</h1>
+    </motion.nav>
+  );
+};
+
+export default Navbar;
